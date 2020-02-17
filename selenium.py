09697fca4124bd2285e02199e0897ad590ea5935
gokul427 <gokulkunigal@gmail.com>
2020-02-17 23:18:33 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -5,4 +5,7 @@ driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 driver.get('http://52.15.241.212:8082/')
 driver.find_element_by_link_text("About Us").click()
 text = driver.find_element_by_class_name("call-to-action").text
-print text
+if text=="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).":
+    return 1
+else:
+    return 0
