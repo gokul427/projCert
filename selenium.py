0bd6903f4dbce31ab4af683eca8833aa72407eba
gokul427 <gokulkunigal@gmail.com>
2020-02-17 22:53:15 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,7 +1,7 @@
 from selenium import webdriver 
 import time
 
-driver = webdriver.Firefox(executable_path='/Users/ghraj/Downloads/geckodriver')
+driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 driver.get('https://python.org')
 driver.find_element_by_link_text("About").click()
 text = driver.find_element_by_class_name("call-to-action").text
