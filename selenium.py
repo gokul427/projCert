d099280fdd11795867b9626c0559848a27bf9eb5
gokul427 <gokulkunigal@gmail.com>
2020-02-17 23:13:09 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
-driver.get('https://python.org')
-driver.find_element_by_link_text("About").click()
+driver.get('http://52.15.241.212:8082/')
+driver.find_element_by_link_text("About Us").click()
 text = driver.find_element_by_class_name("call-to-action").text
 print text
